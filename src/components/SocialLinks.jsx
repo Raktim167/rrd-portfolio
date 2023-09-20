24e0cc83faff061dfa6a25c95a7c68e2274f6af5
Raktim Ranjan Deka <raktim.deka@nativebyte.in>
2023-09-20 19:30:27 +0530

    resume changed

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -31,7 +31,7 @@ const SocialLinks = () => {
                 </a>
             </li>
             <li className='flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500 rounded-br-md'>
-                <a href='/resume.pdf' download={true} className='flex justify-between items-center w-full text-white'>
+                <a href='/resume-Raktim.pdf' download={true} className='flex justify-between items-center w-full text-white'>
                     <>
                     Resume <BsFillPersonLinesFill size={30} />
                     </>
